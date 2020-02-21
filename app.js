ae26aa1f55f1222b9a7936ac338d4e4538d1a677
sinzii <zthangxv@gmail.com>
2020-02-21 22:22:16 +0700

    Fix get static file mime type

--- app.js
+++ app.js
@@ -4,30 +4,29 @@ const mime = require('mime-types');
 
 const ROOT_FOLDER = __dirname;
 
-const app = function(req, res) {
-    const { method, url} = req;
+const app = function (req, res) {
+    const { method, url } = req;
     console.log(method, url);
 
     // TODO: serve static files
     const publicFolder = 'public';
     if (url.startsWith(`/${publicFolder}`)) {
         const targetFile = path.join(ROOT_FOLDER, url);
-        console.log(targetFile);
-        fs.readFile(targetFile, function(err, data) {
+        fs.readFile(targetFile, function (err, data) {
             if (err) {
                 res.statusCode = 404;
                 res.setHeader('Content-Type', 'text/plain');
                 res.end('File not found!');
                 return;
-            } 
-            
-            res.setHeader('Content-Type', mime.contentType(targetFile));
+            }
+
+            res.setHeader('Content-Type', mime.contentType(path.extname(targetFile)));
             res.end(data);
         });
-        
+
         return;
     }
-    
+
 
     // TODO: route to specific path
 
@@ -35,8 +34,9 @@ const app = function(req, res) {
 
 
     res.setHeader('Content-Type', 'text/html');
+
     res.end(`
-    <!doctype html/>
+    <!doctype html>
     <html>
     <head>
         <link rel="stylesheet" type="text/css" href="public/css/style.css"/>
