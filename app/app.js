03e7165ed6e2f400973363c1d9c53cd4dbbbd751
sinzii <thangvx@newayict.com>
2021-05-06 01:09:09 +0700

    Integrate with express-session for session management

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ const router = require('./router');
 const logger = require('log4js').getLogger('app');
 const httpLogger = require('log4js').getLogger('http');
 const { StatusCodes } = require('http-status-codes');
+const session = require('./session');
 
 const staticDirs = ['public'];
 
@@ -12,14 +13,15 @@ const requestHandler = async function (req, res) {
     const {method, url} = req;
     httpLogger.debug(`${method} - ${url}`);
 
-    requestUtils.parseRequestUrl(req);
-    await requestUtils.parseRequestBody(req);
-
     // Serve static files
     const served = staticFileHandler(staticDirs, req, res);
     if (served) {
         return;
     }
+
+    requestUtils.parseRequestUrl(req);
+    await requestUtils.parseRequestBody(req);
+    await session(req, res);
 
     // detect route and serve
     const route = router.getMatchedRoute(req);
---
+++ app/session.js
@@ -0,0 +1,40 @@
+/**
+ * Since there's no session library for pure NodeJs application,
+ * so to save time from building from scratch one.
+ * I decided to use express-session for session management purpose.
+ *
+ * express-session is designed to work with express framework,
+ * so in order for it to work with our application
+ * we need to provide a fake next function as a hack for the library to function smoothly.
+ */
+const config = require('./config');
+const expressSession = require('express-session');
+
+const maxAgeInMinute = config['HTTP_SESSION_MAX_AGE_IN_MINUTE'] || 60;
+
+const sessionFn = expressSession({
+    secret: config['HTTP_SESSION_SECRET'],
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+        httpOnly: true,
+        secure: config.prod,
+        maxAge: maxAgeInMinute * 60 * 1000 // milliseconds
+    }
+});
+
+const session = async (req, res) => {
+    return new Promise((resolve, reject) => {
+        const fakeNext = (err) => {
+            if (err) {
+                reject(new FwlError(err.message));
+            } else {
+                resolve();
+            }
+        }
+
+        sessionFn(req, res, fakeNext);
+    });
+}
+
+module.exports = session;
